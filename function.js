6938d544644ce99aab4c2a9f21aa1d04b1dd2fbb
Braian <braiannickolas12@gmail.com>
2023-12-15 18:04:58 -0300

    operadores estritos
    aula142

--- function.js
+++ function.js
@@ -6,10 +6,18 @@ let one = 1
 let two = 2
 
 // == igual a
-console.log(two == 1)
-console.log(one == "1")
+//console.log(two == 1)
+//console.log(one == "1")
 
 // != diferente de
 // console.log(one != two)
 // console.log(one ! 1)
 // console.log(one != "1")
+
+// === estritamente igual a 
+//console.log(one === "1")
+//console.log(one === 1)
+
+// !== estritamente diferente de
+console.log(two !== "2")
+console.log(two !== 2)
