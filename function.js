dcf5cce17e8d2ef442d4642192e1253b7172985f
Braian <braiannickolas12@gmail.com>
2023-12-15 15:49:07 -0300

    Grouping Operator
    aula140

--- function.js
+++ function.js
@@ -1,30 +1,4 @@
-// Operadores Aritimeticos
+// Grouping operator ( )
 
-// multiplicacao
-//console.log(3.2 * 3)
-
-// divisao
-//console.log(12 / 2)
-
-// soma
-//console.log(34 + 67)
-
-// subtracao
-//console.log(43 - 23)
-
-//resto da divisao
-//let remainder
-//remainder = 11 % 2
-//console.log(remainder)
-
-//incremento
-//let increment = 2
-//increment++
-//console.log(increment)
-
-//decremento
-//let decrement = 0
-//console.log(--decrement)
-
-//exponencial
-console.log(3 ** 2)
+let total = 'alo alo' + 'voce'
+console.log(total)
