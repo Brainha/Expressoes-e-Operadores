c01a255d2f1c2df05f6608e78010b2c6b60527f8
Braian <braiannickolas12@gmail.com>
2023-12-15 18:07:27 -0300

    operadores de comparacao
    aula144

---
+++ function6.js
@@ -0,0 +1,23 @@
+// Operadores de comparacao
+
+// Ira comparar valores e retornar um boolean como resposta a comparacao
+
+let one = 1
+let two = 2
+
+// == igual a
+//console.log(two == 1)
+//console.log(one == "1")
+
+// != diferente de
+// console.log(one != two)
+// console.log(one ! 1)
+// console.log(one != "1")
+
+// === estritamente igual a 
+//console.log(one === "1")
+//console.log(one === 1)
+
+// !== estritamente diferente de
+console.log(two !== "2")
+console.log(two !== 2)
